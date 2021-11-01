f61c6502a656ea454ca68c33c021938d14d2f46a
wizeeee <wizeman22@outlook.com>
2021-11-01 15:24:46 -0400

    Capture user input with command-line arguments

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const animalArray = ["dog", "cat", "pig"];
-
-animalArray.push("cow");
-console.log(animalArray);
-
-const personObj = {
-  name: "Lernantino",
-  age: 99,
-};
-
-personObj.age = 100;
-personObj.occupation = "Developer";
-console.log(personObj);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
