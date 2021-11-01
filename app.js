d9c012f7afb0fa8cfa301d094691c16a6ba5db5c
wizeeee <wizeman22@outlook.com>
2021-10-31 23:26:51 -0400

    testing

---
+++ app.js
@@ -0,0 +1,19 @@
+const animalArray = ["dog", "cat", "pig"];
+
+animalArray.push("cow");
+console.log(animalArray);
+
+const personObj = {
+  name: "Lernantino",
+  age: 99,
+};
+
+personObj.age = 100;
+personObj.occupation = "Developer";
+console.log(personObj);
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
