895b9861a9cb0e5b912ebcab384919ea5ac8bb63
wizeeee <wizeman22@outlook.com>
2021-11-02 23:30:17 -0400

    Expand the HTML template to account for more profile data

--- app.js
+++ app.js
@@ -16,18 +16,10 @@
 
 printProfileData(profileDataArgs); */
 
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
-
 const inquirer = require("inquirer");
+
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -170,5 +162,13 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
